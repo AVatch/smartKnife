e910d22cd2560f2638b072dc3f0efcf3b7b2a4c3
AVatch <akv224@nyu.edu>
2013-09-29 08:22:40 -0700

    hurry

---
+++ KNIFE.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+potato = False
+tomato = False
+
+if(potato):
+	webbrowser.open_new('http://127.0.0.1:8000/result/potato/')
+else:
+	webbrowser.open_new('http://127.0.0.1:8000/result/tomato/')
